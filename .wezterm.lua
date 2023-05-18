1af6da3a8a995b1dfc700de3805d94eff44fa56f
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2023-05-18 19:21:26 +0900

    feat: update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,20 +1,32 @@
 local wezterm = require("wezterm")
 
--- フルスクリーンで起動
+-- full screen on launch
 local mux = wezterm.mux
 wezterm.on('gui-startup', function(cmd)
-    local tab, pane, window = mux.spawn_window(cmd or {})
+    local _, _, window = mux.spawn_window(cmd or {})
     window:gui_window():maximize()
 end)
 
-return {
-    font_size = 15.0,
-    line_height = 0.85,
-    
-    font = wezterm.font_with_fallback({
-        "M+ 1m",
-        "Noto Emoji",
-    }),
-    native_macos_fullscreen_mode = false,
-    window_decorations = "RESIZE",
-}
+
+local config = {}
+local arch = wezterm.target_triple
+if arch == "x86_64-pc-windows-msvc" then
+    -- for Windows
+    config.default_prog = { "PowerShell.exe", "-NoLogo" }
+elseif arch == "x86_64-apple-darwin" or arch == "aarch64-apple-darwin" then
+    -- for Mac
+    config.native_macos_fullscreen_mode = false
+else
+    -- for Linux
+end
+
+-- common settings
+config.font_size = 15.0
+config.line_height = 0.85
+config.font = wezterm.font_with_fallback({
+    "M+ 1m",
+    "Noto Emoji",
+})
+config.window_decorations = "RESIZE"
+
+return config
