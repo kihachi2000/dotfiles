3d639ee431af06f175d42cbe3939dce3cab7eabf
kihachi2000 <kenbayashi.gaming.00@gmail.com>
2023-06-20 15:21:45 +0900

    refactor: remove unused settings

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,10 +1,6 @@
 require("nvim-treesitter.configs").setup({
     highlight = {
         enable = true,
-        disable = {},
         additional_vim_regex_highlighting = false,
-    },
-    indent = {
-        enable = true,
     },
 })
