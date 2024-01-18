d75122427c672a97ae6727a784613b7a03e558f3
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2024-01-18 17:06:10 +0900

    feat: add get_handle()

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -11,6 +11,7 @@ return {
                 null_ls.builtins.diagnostics.eslint,
                 null_ls.builtins.formatting.prettier,
                 null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.rustfmt,
             },
         })
     end,
