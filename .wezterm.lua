6e617f7754fe2320c06c7a588ad5c039c5775894
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2024-01-25 20:39:02 +0900

    feat: add key for SpawnTab

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,31 +2,38 @@ local wezterm = require("wezterm")
 
 -- full screen on launch
 local mux = wezterm.mux
-wezterm.on('gui-startup', function(cmd)
-    local _, _, window = mux.spawn_window(cmd or {})
-    window:gui_window():maximize()
+wezterm.on("gui-startup", function(cmd)
+	local _, _, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
 end)
-
 
 local config = {}
 local arch = wezterm.target_triple
 if arch == "x86_64-pc-windows-msvc" then
-    -- for Windows
-    config.default_prog = { "wsl", "~" }
+	-- for Windows
+	config.default_prog = { "wsl", "~" }
 elseif arch == "x86_64-apple-darwin" or arch == "aarch64-apple-darwin" then
-    -- for Mac
-    config.native_macos_fullscreen_mode = false
+	-- for Mac
+	config.native_macos_fullscreen_mode = false
 else
-    -- for Linux
+	-- for Linux
 end
 
 -- common settings
 config.font_size = 15.0
 config.line_height = 0.85
 config.font = wezterm.font_with_fallback({
-    "M+ 1m",
-    "Noto Emoji",
+	"M+ 1m",
+	"Noto Emoji",
 })
 config.window_decorations = "RESIZE"
+
+config.keys = {
+	{
+		key = "t",
+		mods = "CTRL",
+		action = wezterm.action.SpawnTab("CurrantPaneDomain"),
+	},
+}
 
 return config
