45f158b7d67f2177df6c0e2e325cf7da26fb69c4
kihachi2000 <kenbayashi.gaming.00@gmail.com>
2024-12-01 17:37:25 +0900

    feat: replace none-ls with conform

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,13 @@
+return {
+    'stevearc/conform.nvim',
+    opts = {},
+    config = function()
+        local conform = require("conform")
+
+        conform.setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+            }
+        })
+    end,
+}
