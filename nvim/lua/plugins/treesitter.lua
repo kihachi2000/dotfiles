e755143250076fff930c94796eaee50a2eb6a034
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2024-12-03 17:20:35 +0900

    feat: set ensured_installed for treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,20 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
+            ensured_installed = {
+                "bash",
+                "css",
+                "html",
+                "java",
+                "json",
+                "lua",
+                "markdown",
+                "rust",
+                "toml",
+                "tsx",
+                "typescript",
+                "yaml",
+            },
             highlight = {
                 enable = true,
                 disable = { "vim" },
