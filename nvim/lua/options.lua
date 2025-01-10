8ebf45d68e741b3f0bdb1d8be2b6d78995cd0937
kihachi2000 <kenbayashi.gaming.00@gmail.com>
2025-01-10 11:43:42 +0900

    feat: force nvim to always use OSC52 provider

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -11,6 +11,17 @@ opt.swapfile = false
 
 -- クリップボード
 opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+        ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+        ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+}
 
 -- タブを半角スペース4つにする
 opt.expandtab = true
