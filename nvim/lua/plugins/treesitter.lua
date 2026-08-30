00e868f4d8e34492b4b46055f85af1fe88fb2014
kihachi2000 <58835005+kihachi2000@users.noreply.github.com>
2026-08-30 14:22:12 +0900

    feat(nvim): set lazy false for treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
